2ba14583ac21cc805dfbf7447613d0a213122d5a
victor de souza magaldi <victmagaldi@hotmail.com>
2022-09-17 23:16:15 -0300

    move theme switcher

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,9 @@
-import { ThemeSwitcher } from './ThemeSuitcher';
-
 export default function Footer({ copyrightText }) {
   return (
     <footer className="py-16 flex flex-col items-center">
       <p className="dark:text-white uppercase mb-3 font-bold opacity-60">
         {copyrightText}
       </p>
-      <ThemeSwitcher />
     </footer>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { ThemeSwitcher } from './ThemeSuitcher';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
+      <ThemeSwitcher />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
