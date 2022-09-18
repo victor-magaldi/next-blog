03cdb32d8745266e2a15db15e1c84726e310fac0
victor de souza magaldi <victmagaldi@hotmail.com>
2022-09-17 23:13:32 -0300

    remove footer and header

--- components/Layout.js
+++ components/Layout.js
@@ -53,11 +53,9 @@ export default function Layout({ children }) {
 
   return (
     <div className="relative pb-24 overflow-hidden">
-      <Header />
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {children}
       </div>
-      <Footer />
     </div>
   );
 }
