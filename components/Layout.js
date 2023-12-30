c865f30459744c5044f9945e8c4339b8200032c6
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2023-12-29 23:52:16 -0300

    ajusting container

--- components/Layout.js
+++ components/Layout.js
@@ -53,7 +53,7 @@ export default function Layout({ children }) {
 
   return (
     <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+      <div className="flex flex-col items-center max-w-5xl w-full mx-auto">
         {children}
       </div>
     </div>
